246578de0324ce9bb2ce238cc473524dbfd0624b
Varya-K <126766019+Varya-K@users.noreply.github.com>
2024-09-17 10:32:47 +0300

    Examples of "Command" pattern has been made

--- src/BehavioralDesignPatterns/Command/Demo.java
+++ src/BehavioralDesignPatterns/Command/Demo.java
@@ -1,7 +1,10 @@
 package BehavioralDesignPatterns.Command;
 
+import BehavioralDesignPatterns.Command.editor.Editor;
+
 public class Demo {
     public static void main(String[] args){
-
+        Editor editor = new Editor();
+        editor.init();
     }
 }
