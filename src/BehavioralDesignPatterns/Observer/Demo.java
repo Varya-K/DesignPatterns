33f06a8397212fa53ba557c432f8e25fe990040c
Varya-K <126766019+Varya-K@users.noreply.github.com>
2024-09-20 14:28:14 +0300

    Examples of "Observer" pattern has been made

--- src/BehavioralDesignPatterns/Observer/Demo.java
+++ src/BehavioralDesignPatterns/Observer/Demo.java
@@ -1,7 +1,22 @@
 package BehavioralDesignPatterns.Observer;
 
+import BehavioralDesignPatterns.Observer.editor.Editor;
+import BehavioralDesignPatterns.Observer.listeners.EmailNotificationListener;
+import BehavioralDesignPatterns.Observer.listeners.LogOpenListener;
+
 public class Demo {
     public static void main(String[] args){
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("logfile.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try{
+            editor.openFile("text.txt");
+            editor.saveFile();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/BehavioralDesignPatterns/Observer/listeners/EmailNotificationListener.java
@@ -0,0 +1,16 @@
+package BehavioralDesignPatterns.Observer.listeners;
+
+import java.io.File;
+
+public class EmailNotificationListener implements EventListener{
+    private String email;
+
+    public EmailNotificationListener(String email){
+        this.email=email;
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Email to "+email+": someone has performed "+eventType+" operation with the following file: "+file.getName());
+    }
+}
